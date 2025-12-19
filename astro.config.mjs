eab784b21394d4d399b7a8c75053885fc0e963e5
memoriastudio <memoristudiositiowe@gmail.com>
2025-12-19 13:02:13 -0300

    build: Update Vercel adapter import from `vercelServerless` to `vercelAdapter` in Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercelAdapter from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), compress()],
-  adapter: vercelServerless(),
+  adapter: vercelAdapter(),
   output: 'server',
 });
