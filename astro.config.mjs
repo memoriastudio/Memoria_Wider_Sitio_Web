38b55a4f435c4a3b2c225224fd832fc32de2c716
memoriastudio <memoristudiositiowe@gmail.com>
2025-12-19 12:56:47 -0300

    build: switch Astro Vercel adapter to serverless.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-import vercel from '@astrojs/vercel/server';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), compress()],
-  adapter: vercel(),
+  adapter: vercelServerless(),
   output: 'server',
 });
