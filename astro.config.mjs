75f82b0028abd33ce15efdd9bba26eb36c915603
memoriastudio <memoristudiositiowe@gmail.com>
2025-12-19 13:15:43 -0300

    refactor: update Vercel adapter import path and usage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-import vercelAdapter from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/server';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress()],
-  adapter: vercelAdapter(),
   output: 'server',
+  adapter: vercel(),
+  integrations: [
+    tailwind(),
+    compress(),
+  ],
 });
