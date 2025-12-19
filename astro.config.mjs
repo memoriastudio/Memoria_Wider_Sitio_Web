2a37244591e88d0a6eff78acacfe313943cba6ff
memoriastudio <memoristudiositiowe@gmail.com>
2025-12-19 13:26:45 -0300

    chore: Update Vercel adapter import path in Astro configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-import vercel from '@astrojs/vercel/server';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
