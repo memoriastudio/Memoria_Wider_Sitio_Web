dd0d3ceeef1e8cdf3503ecae17838dd4e656a9fd
memoriastudio <memoristudiositiowe@gmail.com>
2025-12-31 10:44:50 -0300

    Update SEO and performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://wider.memoriastudio.net',
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind(), compress()],
+  integrations: [tailwind(), compress(), sitemap()],
 });
