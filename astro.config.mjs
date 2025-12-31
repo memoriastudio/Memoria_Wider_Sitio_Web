446a21202192e63c51ade8cbf6c2824963732ddf
memoriastudio <memoristudiositiowe@gmail.com>
2025-12-31 10:28:47 -0300

    url site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import compress from 'astro-compress';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  site: 'https://wider.memoriastudio.net',
   output: 'server',
   adapter: vercel(),
   integrations: [tailwind(), compress()],
